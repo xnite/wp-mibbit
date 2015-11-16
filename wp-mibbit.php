b0f3016b9d3614043afd8f263c7d2df782a781a9
Robert Whitney <xnite@users.noreply.github.com>
2015-11-16 17:19:06 -0600

    Update default network address & author email+URI

--- wp-mibbit.php
+++ wp-mibbit.php
@@ -3,10 +3,10 @@
 Plugin Name: WP-Mibbit
 Plugin URI: https://github.com/xnite/wp-mibbit
 Description: Allows you to embed a Mibbit IRC client with a shortcode.
-Version: 1.0
-Author: Robert Whitney &lt;<a href="mailto:xnite@xnite.org">xnite@xnite.org</a>&gt;
-Author URI: http://xnite.org
-License: http://xnite.org/copyright#code
+Version: 1.1
+Author: Robert Whitney &lt;<a href="mailto:xnite@xnite.me">xnite@xnite.me</a>&gt;
+Author URI: https://xnite.me
+License: GNU General Public License
 */
 
 function mibbit_embed_func($atts) {
@@ -14,7 +14,7 @@ function mibbit_embed_func($atts) {
 		'width'		=> '95%',
 		'height'	=> '450px',
 		'settings'	=> '',
-		'server'	=> 'irc.afraidirc.net',
+		'server'	=> 'irc.altsociety.co',
 		'channel'	=> '#lobby'
 	), $atts));
 	return '<iframe style="width:'.$width.';height:'.$height.';border:0;" src="https://widget.mibbit.com/?settings='.$settings.'&server='.$server.'&channel='.urlencode($channel).'"></iframe>';
